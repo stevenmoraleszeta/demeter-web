1ccaf6ada2a1d506a89480e2e4bebd3fd854b6dd
Steven Morales <fallasmoraless@gmail.com>
2024-08-02 18:28:59 -0600

    Improve Langing Page Desing and Implement Demo Prototype

--- src/Pages/DemoWeb/DemeterDemo.jsx
+++ src/Pages/DemoWeb/DemeterDemo.jsx
@@ -1,65 +1,265 @@
 import React, { useState } from 'react';
 import '../../App.css';
 import './DemeterDemo.css';
-
-const initialZones = [
-    { id: 1, name: 'Zona 1', temperature: 22, humidity: 55, nutrients: 'Adecuado' },
-    { id: 2, name: 'Zona 2', temperature: 24, humidity: 60, nutrients: 'Alto' },
-    { id: 3, name: 'Zona 3', temperature: 21, humidity: 50, nutrients: 'Bajo' },
-];
+import fincaImagenArriba from '../../Assets/Img/fincaImagenArriba.png'; // Importa la imagen
 
 function DemeterDemo() {
-    const [zones, setZones] = useState(initialZones);
-    const [irrigationActive, setIrrigationActive] = useState(false);
-    const [inventory, setInventory] = useState({
-        water: 1000,
-        fertilizer: 500,
-        seeds: 200,
-    });
+    const [activeZone, setActiveZone] = useState(null);
 
-    const toggleIrrigation = () => {
-        setIrrigationActive(!irrigationActive);
+    const zones = [
+        {
+            name: 'Zona 1', gridArea: '1 / 1 / 2 / 2', data: {
+                title: 'Zona 1',
+                indicators: [
+                    'Tipo de plantación: Tomates',
+                    'Última fecha y hora de monitoreo: 01-08-2024 10:00 AM',
+                    'Última fecha y hora de riego: 30-07-2024 04:00 PM',
+                    'Temperatura encontrada: 20°C',
+                    'Temperatura ideal: 18-22°C',
+                    'Humedad: 65%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Normal',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Bajo',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Normal',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Ninguna',
+                    'Sospecha de plaga: Ninguna'
+                ]
+            }
+        },
+        {
+            name: 'Zona 2', gridArea: '1 / 2 / 2 / 3', data: {
+                title: 'Zona 2',
+                indicators: [
+                    'Tipo de plantación: Lechugas',
+                    'Última fecha y hora de monitoreo: 01-08-2024 11:00 AM',
+                    'Última fecha y hora de riego: 29-07-2024 06:00 PM',
+                    'Temperatura encontrada: 22°C',
+                    'Temperatura ideal: 18-22°C',
+                    'Humedad: 70%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Bajo',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Normal',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Bajo',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Leve',
+                    'Sospecha de plaga: Alta'
+                ]
+            }
+        },
+        {
+            name: 'Zona 3', gridArea: '1 / 3 / 2 / 4', data: {
+                title: 'Zona 3',
+                indicators: [
+                    'Tipo de plantación: Pimientos',
+                    'Última fecha y hora de monitoreo: 01-08-2024 12:00 PM',
+                    'Última fecha y hora de riego: 28-07-2024 05:00 PM',
+                    'Temperatura encontrada: 25°C',
+                    'Temperatura ideal: 20-25°C',
+                    'Humedad: 60%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Normal',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Normal',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Alto',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Ninguna',
+                    'Sospecha de plaga: Leve'
+                ]
+            }
+        },
+        {
+            name: 'Zona 4', gridArea: '1 / 4 / 2 / 5', data: {
+                title: 'Zona 4',
+                indicators: [
+                    'Tipo de plantación: Zanahorias',
+                    'Última fecha y hora de monitoreo: 01-08-2024 01:00 PM',
+                    'Última fecha y hora de riego: 27-07-2024 07:00 PM',
+                    'Temperatura encontrada: 27°C',
+                    'Temperatura ideal: 22-27°C',
+                    'Humedad: 55%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Alto',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Normal',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Bajo',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Alta',
+                    'Sospecha de plaga: Ninguna'
+                ]
+            }
+        },
+        {
+            name: 'Zona 5', gridArea: '2 / 1 / 3 / 2', data: {
+                title: 'Zona 5',
+                indicators: [
+                    'Tipo de plantación: Cebollas',
+                    'Última fecha y hora de monitoreo: 02-08-2024 10:00 AM',
+                    'Última fecha y hora de riego: 01-08-2024 04:00 PM',
+                    'Temperatura encontrada: 18°C',
+                    'Temperatura ideal: 15-20°C',
+                    'Humedad: 75%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Bajo',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Bajo',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Normal',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Ninguna',
+                    'Sospecha de plaga: Moderada'
+                ]
+            }
+        },
+        {
+            name: 'Zona 6', gridArea: '2 / 2 / 3 / 3', data: {
+                title: 'Zona 6',
+                indicators: [
+                    'Tipo de plantación: Calabacines',
+                    'Última fecha y hora de monitoreo: 02-08-2024 11:00 AM',
+                    'Última fecha y hora de riego: 31-07-2024 06:00 PM',
+                    'Temperatura encontrada: 23°C',
+                    'Temperatura ideal: 20-25°C',
+                    'Humedad: 68%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Normal',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Bajo',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Normal',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Leve',
+                    'Sospecha de plaga: Ninguna'
+                ]
+            }
+        },
+        {
+            name: 'Zona 7', gridArea: '2 / 3 / 3 / 4', data: {
+                title: 'Zona 7',
+                indicators: [
+                    'Tipo de plantación: Espinacas',
+                    'Última fecha y hora de monitoreo: 02-08-2024 12:00 PM',
+                    'Última fecha y hora de riego: 30-07-2024 05:00 PM',
+                    'Temperatura encontrada: 24°C',
+                    'Temperatura ideal: 20-25°C',
+                    'Humedad: 62%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Normal',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Normal',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Bajo',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Moderada',
+                    'Sospecha de plaga: Baja'
+                ]
+            }
+        },
+        {
+            name: 'Zona 8', gridArea: '2 / 4 / 3 / 5', data: {
+                title: 'Zona 8',
+                indicators: [
+                    'Tipo de plantación: Judías verdes',
+                    'Última fecha y hora de monitoreo: 02-08-2024 01:00 PM',
+                    'Última fecha y hora de riego: 29-07-2024 07:00 PM',
+                    'Temperatura encontrada: 26°C',
+                    'Temperatura ideal: 22-27°C',
+                    'Humedad: 58%',
+                    'Humedad ideal: 60-70%',
+                    'Nitrogeno: Alto',
+                    'Nitrogeno ideal: 80-100 ppm',
+                    'Fosforo: Bajo',
+                    'Fosforo ideal: 30-50 ppm',
+                    'Potacio: Normal',
+                    'Potacio ideal: 150-200 ppm',
+                    'Sospecha de enfermedad: Ninguna',
+                    'Sospecha de plaga: Moderada'
+                ]
+            }
+        }
+    ];
+    
+    
+
+    const determineZoneState = (indicators) => {
+        const temp = parseInt(indicators.find(ind => ind.includes('Temperatura encontrada')).split(': ')[1]);
+        const idealTempRange = indicators.find(ind => ind.includes('Temperatura ideal')).split(': ')[1].split('-').map(Number);
+        const humidity = parseInt(indicators.find(ind => ind.includes('Humedad')).split(': ')[1]);
+        const idealHumidityRange = [40, 60]; // Ejemplo de rango ideal
+
+        // Definir márgenes para estado amarillo
+        const tempMargin = 2; // Rango de margen para temperatura en estado amarillo
+        const humidityMargin = 10; // Rango de margen para humedad en estado amarillo
+
+        if (temp < idealTempRange[0] - tempMargin || temp > idealTempRange[1] + tempMargin ||
+            humidity < idealHumidityRange[0] - humidityMargin || humidity > idealHumidityRange[1] + humidityMargin) {
+            return 'red'; // Estado rojo si está fuera del rango ideal
+        } else if (temp < idealTempRange[0] || temp > idealTempRange[1] ||
+            humidity < idealHumidityRange[0] || humidity > idealHumidityRange[1]) {
+            return 'yellow'; // Estado amarillo si está cerca del límite ideal
+        }
+        return 'green'; // Estado verde si todo está en el rango ideal
     };
 
-    const updateZoneInfo = (id, data) => {
-        setZones(zones.map(zone =>
-            zone.id === id ? { ...zone, ...data } : zone
-        ));
+    const handleMouseMove = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        // Encuentra la zona correspondiente
+        const gridX = Math.floor((x / rect.width) * 4) + 1; // Divide en 4 columnas
+        const gridY = Math.floor((y / rect.height) * 2) + 1; // Divide en 2 filas
+
+        const data = zones.find(zone => {
+            const [rowStart, colStart, rowEnd, colEnd] = zone.gridArea.split(' / ').map(Number);
+            return gridX >= colStart && gridX < colEnd && gridY >= rowStart && gridY < rowEnd;
+        });
+
+        setActiveZone(data ? { ...data.data, state: determineZoneState(data.data.indicators) } : null);
     };
 
     return (
-        <div className="demo-web">
-            <header className="demo-header">
-                <h1>Demeter - Plataforma de Gestión de Finca</h1>
-                <button onClick={toggleIrrigation} className="irrigation-button">
-                    {irrigationActive ? 'Desactivar Riego' : 'Activar Riego'}
-                </button>
-            </header>
-
-            <section className="zones-info">
-                <h2>Información de Zonas</h2>
-                {zones.map(zone => (
-                    <div key={zone.id} className="zone">
-                        <h3>{zone.name}</h3>
-                        <p><strong>Temperatura:</strong> {zone.temperature}°C</p>
-                        <p><strong>Humedad:</strong> {zone.humidity}%</p>
-                        <p><strong>Nutrientes:</strong> {zone.nutrients}</p>
-                        <button onClick={() => updateZoneInfo(zone.id, { temperature: zone.temperature + 1 })}>
-                            Aumentar Temperatura
-                        </button>
-                        <button onClick={() => updateZoneInfo(zone.id, { humidity: zone.humidity + 5 })}>
-                            Aumentar Humedad
-                        </button>
+        <div className="demo-container">
+            <div className="grid-container" onMouseMove={handleMouseMove}>
+                <img
+                    src={fincaImagenArriba}
+                    alt="Mapa de Finca"
+                    className="demo-image"
+                />
+                {zones.map((zone, index) => (
+                    <div
+                        key={index}
+                        className={`zone ${activeZone && activeZone.title === zone.data.title ? `zone-${activeZone.state}` : ''}`}
+                        style={{ gridArea: zone.gridArea }}
+                    >
+                        {zone.name}
                     </div>
                 ))}
-            </section>
-
-            <section className="inventory-management">
-                <h2>Manejo del Inventario</h2>
-                <p><strong>Agua:</strong> {inventory.water} L</p>
-                <p><strong>Fertilizante:</strong> {inventory.fertilizer} kg</p>
-                <p><strong>Semillas:</strong> {inventory.seeds} unidades</p>
-            </section>
+            </div>
+            <div className="zone-data-section">
+                {activeZone ? (
+                    <div className="zone-data">
+                        <h1>{activeZone.title}</h1>
+                        <ul>
+                            {activeZone.indicators.map((item, index) => (
+                                <ul key={index} className='indicador-container'>
+                                    <li>
+                                        <strong>{item.split(':')[0]}:</strong></li>
+                                    <li>{item.split(':')[1]}</li>
+                                </ul>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p>Posicione el cursor sobre una zona</p>
+                )}
+            </div>
         </div>
     );
 }
--- src/Pages/landingPage/LandingPage.jsx
+++ src/Pages/landingPage/LandingPage.jsx
@@ -6,41 +6,37 @@ function LandingPage() {
     return (
         <>
             <header className="page-header">
-                <h1 className='header-title'>Demeter</h1>
+                <h1 className='header-title'>DEMETER</h1>
                 <p className="header-pharse">Revoluciona la agricultura con tecnología avanzada</p>
                 <a href="#demeter-info" className="header-button">Descubre Más</a>
             </header>
 
             <section id="demeter-info" className="demeter-info">
-                <h2 className="demeter-info-description">Demeter es un sistema avanzado diseñado para transformar la agricultura mediante la integración de tecnología y automatización. Utilizando dispositivos de recolección de datos y sistemas automatizados, Demeter optimiza todo el proceso de producción alimentaria. Nuestro objetivo es proporcionar alimentos de alta calidad a todas las personas, al mismo tiempo que promovemos prácticas agrícolas sostenibles y apoyamos a la comunidad agrícola. Al mejorar la eficiencia y reducir el impacto ambiental, Demeter contribuye a un futuro más saludable y sostenible para todos.</h2>
+                <h2 className="demeter-info-description">Demeter es un sistema avanzado que transforma la agricultura mediante tecnología y automatización. Optimiza la producción alimentaria con dispositivos de recolección de datos y sistemas automatizados. Nuestro objetivo es proporcionar alimentos de alta calidad, promoviendo prácticas agrícolas sostenibles y apoyando a la comunidad agrícola. Demeter minimiza el uso de recursos naturales, reduce la huella de carbono y protege la biodiversidad, mejorando la eficiencia y evitando la contaminación. Empoderamos a los agricultores locales con acceso a tecnologías avanzadas y datos precisos, promoviendo la equidad y el desarrollo económico. Con Demeter, la agricultura del futuro es verde, justa y próspera.</h2>
             </section>
 
             <section className="devices-container">
                 <div className="device">
-                    <h2>Sensor Terrestre</h2>
-                    <p>Recolección de datos, humedad, temperatura, nutrientes.</p>
-                </div>
-                <div className="device">
-                    <h2>Sistema de Riego Inteligente</h2>
-                    <p>Automatización y eficiencia del riego de agua y otros compuestos.</p>
+                    <h2>Dispositivo Hormiga</h2>
+                    <p>Recolección de datos (humedad, temperatura, nutrientes, enfermedades, plagas).</p>
                 </div>
                 <div className="device">
                     <h2>Sistema de Administración</h2>
-                    <p>Manejo de cultivos, inventarios, zonas, gráficos, fechas, tareas, entre otros.</p>
+                    <p>Visualización y análisis de datos.</p>
+                    <p>Manejo de cultivos, inventarios, zonas, fechas, tareas, entre otros.</p>
                 </div>
             </section>
-
             <section className="demo-access">
-                <h2>Prueba Nuestra Plataforma</h2>
-                <a href="/demeter-demo" className="demo-button" target="_blank" rel="noopener noreferrer">Acceder a la Demo Web</a>
+                <h1>Prueba el Simulador del Sistema Demeter</h1>
+                <p className="demo-access-description">El prototipo está diseñado para ilustrar cómo funcionaría el prototipo en una finca real, brindando una visión clara y práctica de su impacto y operación en un entorno agrícola auténtico.</p>
+                <a href="/demeter-demo" className="demo-button" target="_blank" rel="noopener noreferrer">Acceder al Prototipo</a>
             </section>
-
             <section className="video-demo">
-                <h2>Video Demo</h2>
+                <h2>Mi Proyecto en Dos Minutos</h2>
                 <div className="video-container">
                     <iframe
-                        width="560"
-                        height="315"
+                        width="100%"
+                        height="500"
                         src="https://www.youtube.com/embed/mrjzUFPloiY"
                         title="Video Demo"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
